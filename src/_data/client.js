868ebd4746bc6192fd13bead4d1a61e8b6cc117a
djrdog578 <jrogero578@gmail.com>
2025-02-15 13:20:12 -0500

    2nd commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Oconee CPA",
+    email: "Chandler@OconeeCPA.com",
+    phoneForTel: "478-538-3988",
+    phoneFormatted: "(478) 538-3988",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        lineOne: "1000 Oconee Street",
+        lineTwo: "Suite 100",
+        city: "Watkinsville",
+        state: "GA",
+        zip: "30677",
         country: "US",
         mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
     },
@@ -17,5 +17,5 @@ module.exports = {
         instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.oconeecpa.com",
 };
