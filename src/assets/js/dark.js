aadce5620227dcf0df87c1d4090a4d120e75bff1
djrdog578 <jrogero578@gmail.com>
2025-02-21 11:11:59 -0500

    1st draft

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -14,23 +14,17 @@ function disableDarkMode() {
 
 // determines a new users dark mode preferences
 function detectColorScheme() {
-    // default to the light theme
-    let theme = "light";
+    // default to dark theme
+    let theme = "dark";
 
-    // check localStorage for a saved 'theme' variable. if it's there, the user has visited before, so apply the necessary theme choices
+    // check localStorage for a saved 'theme' variable
     if (localStorage.getItem("theme")) {
         theme = localStorage.getItem("theme");
     }
-    // if it's not there, check to see if the user has applied dark mode preferences themselves in the browser
-    else if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-        theme = "dark";
-    }
-
-    // if there is no preference set, the default of light will be used. apply accordingly
-    theme === "dark" ? enableDarkMode() : disableDarkMode();
+    // if no saved preference, keep dark mode as default
+    
+    // apply the theme
+    theme === "light" ? disableDarkMode() : enableDarkMode();
 }
 
 // run on page load
